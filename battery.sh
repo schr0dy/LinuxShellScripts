a9875674522382bfcb4068d318901a9668e42850
schr0dy <93306565+schr0dy@users.noreply.github.com>
2023-01-09 16:14:29 +0000

    Battery script better color
    Battery Script now see how much battery is left, and if it's less than 20%, it will show the print in yellow.

--- battery.sh
+++ battery.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 state=$(upower -i `upower -e | grep 'BAT'` | grep state | awk '{print $2}')
-battery=$(upower -i `upower -e | grep 'BAT'` | grep 'percentage' | awk '{print $2}')
+battery=$(upower -i `upower -e | grep 'BAT'` | grep percentage | awk '{print $2}')
+percentage=$(upower -i `upower -e | grep 'BAT'` | grep percentage | tr -d % | awk '{print $2}')
+
 if [ $state == 'discharging' ]; then
-	echo "%{F#ff0000}$battery%{u-}"
+	if [ $percentage -lt '20' ]; then
+		echo "%{F#fcec03}$battery%{u-}"
+	else
+		echo "%{F#ff0000}$battery%{u-}"
+	fi
 else
-	echo "%{F#00ff00}$battery%{u-}"
+	if [ $battery == '100%' ]; then
+		echo "%{F#2ccce4}$battery%{u-}"
+	else	
+		echo "%{F#00ff00}$battery%{u-}"
+	fi
 fi
